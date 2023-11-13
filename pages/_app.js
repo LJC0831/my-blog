5e6acf5f0ef54e93be506f0456ac6b36d8ffcd71
LEEJAECHUN <leejc831@naver.com>
2023-11-13 17:42:36 +0900

    타이틀수정

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  <Head>
+    {/* 초기title */}
+      <title>LJC 개발 블로그</title>
+  </Head>
   return <Component {...pageProps} />
 }
 

f17806b72f883122e83f32e8ffbeeda3eabd1e88
leejc <leejc831@naver.com>
2023-11-25 22:03:23 +0900

    설정

--- next.config.js
+++ next.config.js
@@ -4,4 +4,15 @@ module.exports = {
     loader: 'imgix',
     path: '/',
   },
+
+  exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
+    // 정적으로 렌더링할 페이지 경로 설정
+    const pagesToExport = { 
+      '/': { page: '/' }, 
+      // 다른 페이지들...
+      // '/board/BoardWrite': { page: '/board/BoardWrite' }, // 이 부분을 주석 처리하여 제외
+    };
+
+    return pagesToExport;
+  },
 };

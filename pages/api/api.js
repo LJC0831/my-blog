670a3dd2a8e328dc8488e75e3d0e20798f4a1c19
LEEJAECHUN <leejc831@naver.com>
2024-01-29 14:54:15 +0900

    토스트 설치

--- pages/api/api.js
+++ pages/api/api.js
@@ -1,3 +1,4 @@
+import { toast } from 'react-toastify';
 //const api = 'https://backendserver.shop:3000';
 const api = 'https://port-0-backend-nodejs-20zynm2mlk2nnlwj.sel4.cloudtype.app';
 //const api = 'http://localhost:3000';
@@ -7,18 +8,16 @@ export function exchangeString01(text){ //  따옴표변환
 }
 
 // toast 메세지 처리
-export function showToast(vm, data) {
-    vm.$toast.open({
-        message: data,
-        duration: 2000, // 토스트 메시지가 보여지는 시간 (2초)
-        position: 'top', // 토스트 메시지 위치
-        type: 'info', // 토스트 메시지 타입 (info, success, error)
-        style: {
-          background: '#888', // 회색 배경 색상
-          'font-size': '14px', // 작은 글꼴 크기
-          color: '#fff', // 흰색 글자 색상
-        },
-      });
-}
+export const showToast = (data) => {
+    toast.info(data, {
+      autoClose: 2000, // 토스트 메시지가 보여지는 시간 (2초)
+      position: 'top-center', // 토스트 메시지 위치
+      style: {
+        background: '#888', // 회색 배경 색상
+        fontSize: '14px', // 작은 글꼴 크기
+        color: '#fff', // 흰색 글자 색상
+      },
+    });
+  };
 
 export default api;

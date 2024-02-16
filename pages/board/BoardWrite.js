f446b2d2e37364b4264b45f7b82a4adabc7fba2d
leejc <leejc831@naver.com>
2024-02-16 23:41:06 +0900

    테스트

--- pages/board/BoardWrite.js
+++ pages/board/BoardWrite.js
@@ -326,18 +326,18 @@ export async function getServerSideProps(context) {
   try {
     if (!isNaN(id)) {
       const data = await Search01(id, 'Admin'); //게시글조회
-      if (data) {
-        seo_title = data[0].title;
-        seo_privew = data[0].privew_content;
-        seo_Thumbnail = data[0].thumbnail_url;
-        return {
-          props: {
-            seo_title,
-            seo_privew,
-            seo_Thumbnail,
-          },
-        };
-      }
+      // if (data) {
+      //   seo_title = data[0].title;
+      //   seo_privew = data[0].privew_content;
+      //   seo_Thumbnail = data[0].thumbnail_url;
+      //   return {
+      //     props: {
+      //       seo_title,
+      //       seo_privew,
+      //       seo_Thumbnail,
+      //     },
+      //   };
+      // }
     }
   } catch(error){
     return {

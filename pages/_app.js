af04ce25b35505ebe1e18803f5a0c19861b51db2
LEEJAECHUN <leejc831@naver.com>
2024-04-05 09:37:38 +0900

    로딩처리

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
+import { useEffect } from "react";
+import Router from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 import '../styles/globals.css'
 import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const start = () => {
+      NProgress.start();
+    };
+    const end = () => {
+      NProgress.done();
+    };
+
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", end);
+    Router.events.on("routeChangeError", end);
+
+    return () => {
+      Router.events.off("routeChangeStart", start);
+      Router.events.off("routeChangeComplete", end);
+      Router.events.off("routeChangeError", end);
+    };
+  }, []);
   <Head>
     {/* 초기title */}
       <title>LJC 개발 블로그</title>
